041cb07ec15f3dd7f31bbdff3521c3022797cb35
Yan9819 <yanvasco098@gmail.com>
2025-06-21 15:10:55 -0300

    Update update_tarefa.php

--- Questão-2/update_tarefa.php
+++ Questão-2/update_tarefa.php
@@ -1,1 +1,11 @@
+<?php
+include 'database.php';
+
+if (isset($_GET['id'])) {
+    $stmt = $db->prepare("UPDATE tarefas SET concluida = 1 WHERE id = ?");
+    $stmt->execute([$_GET['id']]);
+}
+
+header("Location: index.php");
+exit;
 
